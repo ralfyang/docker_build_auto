921ebf94ffad849efc121eeec22249aaf583f977
Ralf Yang <goody80>
2015-08-06 11:55:16 +0900

    update for 0.3 version

--- docker_build_auto.sh
+++ docker_build_auto.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 ### docker image build automation with verification
 ### Made by ralf.yang@gsshop.com, goody80762@gmail.com
+### Version 0.3
 
+Docker_repo=`cat /data/z/etc/init.d/docker | grep "^connection" | awk -F '=' '{print $2}' | sed -e 's/"//g' `
 
-## YOU CAN CHANGE THE COMMAND for WHAT YOU WANT AS BELOW CheckCommand.
-CheckCommand="rpm -qa |grep sshd"
+## YOU CAN CHANGE THE COMMAND for WHAT YOU NEED AS BELOW CheckCommand.
+CheckCommand="ls -lai"
 
 Output="/tmp/build_auto.list"
 BAR="====================================="
 
+VerifiedTag="VFD"
+
+WorkDir="$PWD"
+OutputDir="$WorkDir/Output"
 ## Please modify under variable for the private repository if you have.
 sudo_pkg_check=`rpm -qa  2> /dev/null |grep "^sudo"`
 	if [[ $sudo_pkg_check = "" ]]; then
@@ -18,7 +24,7 @@ sudo_pkg_check=`rpm -qa  2> /dev/null |grep "^sudo"`
 	fi
 
 
-ImagesList=(`ls -d */ | sed -e 's#/##g'`)
+ImagesList=(`ls -d */ |egrep -v "Output" | sed -e 's#/##g'`)
 	if [[ $ImagesList = "" ]];then
 		echo ""
 		echo " === Please make sure the target Dockerfile or directory ! ==="
@@ -42,12 +48,18 @@ read build_num
 
 ## Select a number for work
 DockerName=`grep "^$build_num" $Output | awk '{print $3}'`
-DockerNameOut="$DockerName.out"
+
+mkdir $OutputDir -p 2> /dev/null
+
+DockerNameOut="$OutputDir/$DockerName.out"
+DockerUplog="$OutputDir/$DockerName.log"
 ErrorOut="buildauto.err"
 EnablePortArry=(`cat ./$DockerName/Dockerfile |grep EXPOSE | sed -e 's/EXPOSE //g'`)
+Tags=`cat ./$DockerName/Dockerfile |grep "^#Tags=" | awk -F'=' '{print $2}'`
+
 
 ## Check build success
-$Comm build -t $DockerName ./$DockerName |tee $DockerNameOut
+$Comm build -t $DockerName:$Tags ./$DockerName |tee $DockerNameOut
 	if [[ `(grep  "Successfully built" $DockerNameOut)` = "" ]];then
 		echo "$BAR"
 		echo "Build failed!!!"
@@ -56,26 +68,31 @@ $Comm build -t $DockerName ./$DockerName |tee $DockerNameOut
 	fi
 
 ## Running docker for check
-$Comm run -d --name $DockerName $DockerName tailf /etc/resolv.conf
+$Comm run -d --name $DockerName $DockerName:$Tags tailf /etc/resolv.conf
 	if [[ `($Comm exec $DockerName $CheckCommand)` = "" ]];then
 		echo "$BAR"
-		echo " Pacakge dose not install in Docker image yet!!"
+		echo " Zinst pacakge dose not install in Docker image yet!!"
 		echo "$BAR"
 		exit 0;
 	fi
 
 
 	if [[ ${EnablePortArry[@]} != "" ]];then
-		if [[ `($Comm ps |grep ${EnablePortArry[0]})` = "" ]];then
-			echo ""
-			echo "$BAR"
-			echo "EXPOSE Port has not attented"
-			echo "$BAR"
-			exit 0;
-		else
-			echo " === Port EXPOSE check==="
-			$Comm ps |grep ${EnablePortArry[0]}
-		fi
+		#countPort=0
+		#while [ $countPort -lt ${#EnablePortArry[@]} ];do
+			#$Comm exec -it $DockerName netstat -antp |grep ${EnablePortArry[$countPort]}
+			if [[ `($Comm ps |grep ${EnablePortArry[0]})` = "" ]];then
+				echo ""
+				echo "$BAR"
+				echo "EXPOSE Port has not attented"
+				echo "$BAR"
+				exit 0;
+			else
+				echo " === Port EXPOSE check==="
+				$Comm ps |grep ${EnablePortArry[0]}
+			fi
+		#let countPort=$countPort+1
+		#done
 	fi
 
 
@@ -85,19 +102,62 @@ $Comm stop $DockerName
 $Comm rm $DockerName
 
 echo ""
-ImgChk=`$Comm images |grep "verified/$DockerName"`
+
+ImgChk=`$Comm images |grep "^$VerifiedTag/$DockerName"`
 	if [[ $ImgChk != "" ]];then
 		echo "$BAR"
 		echo "Image name already existed!"
 		echo "$BAR"
+		#$Comm rmi  -f "$VerifiedTag//$DockerName"
 	fi
 echo " Running new container for tagging new...."
 
-$Comm tag $DockerName verified/$DockerName
+$Comm tag $DockerName:$Tags $VerifiedTag/$DockerName:$Tags
 
 echo "$BAR"
 echo " Good job!  everything is okay!"
-$Comm images |grep "verified/$DockerName"
+$Comm images |grep "^$VerifiedTag/$DockerName"
 echo "$BAR"
-rm -f $DockerNameOut $ErrorOut
+
+rm -f $DockerNameOut
+
+$Comm tag $VerifiedTag/$DockerName:$Tags $Docker_repo/$VerifiedTag/$DockerName:$Tags
+$Comm tag $Docker_repo/$VerifiedTag/$DockerName:$Tags $Docker_repo/$VerifiedTag/$DockerName:latest
+$Comm push $Docker_repo/$VerifiedTag/$DockerName:$Tags |tee $ErrorOut
+$Comm push $Docker_repo/$VerifiedTag/$DockerName:latest |tee $ErrorOut
+
+
+## Check upload
+        if [[ `(grep  "connection refused" $ErrorOut)` != "" ]];then
+                echo "$BAR"
+                echo " Upload failed!!! You should check a Registry."
+                echo "$BAR"
+		$Comm rmi -f $Docker_repo/$VerifiedTag/$DockerName:$Tags
+		$Comm rmi -f $VerifiedTag/$DockerName:$Tags
+		$Comm rmi -f $Docker_repo/$VerifiedTag/$DockerName:latest
+                exit 0;
+        fi
+## Upload log delete
+rm -fR $ErrorOut
+
+### Parsing for IP added address
+#curl http://$Docker_repo/v2/_catalog 2> /dev/null | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed -e 's#\[##g' -e 's#\]##g' -e 's#"repositories"\:##g' -e "s#^\"#$Docker_repo/#g" -e 's/"//g' |sort --version-sort |grep "/gs/"
+
+	ImgArry=(`curl http://$Docker_repo/v2/_catalog 2> /dev/null | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed -e 's#\[##g' -e 's#\]##g' -e 's#"repositories"\:##g'  -e 's/"//g' | grep "$VerifiedTag/"`)
+	Counter=0
+	TempArry=./temp
+	ResultList=$WorkDir/available_image.list
+	touch $TempArry
+	while [ $Counter -lt ${#ImgArry[@]} ];do
+		curl "http://$Docker_repo/v2/${ImgArry[$Counter]}/tags/list" 2> /dev/null | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' |sed -e 's/"//g' >> $TempArry
+		cat $TempArry 2> /dev/null | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed -e 's/"//g' -e 's/name:/\n---/g' -e 's/tags://g' -e 's/\[//g' -e 's/\]//g' > $ResultList
+	let Counter=$Counter+1
+	done
+rm -Rf $TempArry $OutputDir 2> /dev/null
+
+$Comm rmi -f $Docker_repo/$VerifiedTag/$DockerName:$Tags
+$Comm rmi -f $VerifiedTag/$DockerName:$Tags
+$Comm rmi -f $Docker_repo/$VerifiedTag/$DockerName:latest
+
+cd $WorkDir/../; git pull;git add *;git commit -m "update the Available list for - $Docker_repo/$VerifiedTag/$DockerName:$Tags" ; git push
 
